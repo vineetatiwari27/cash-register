9ed46cd2a1c3da6cf55f875fffa16390aafae779
vineetatiwari27 <85996556+vineetatiwari27@users.noreply.github.com>
2021-09-24 17:57:37 +0530

    docs: changes in documentation

---
+++ app.js
@@ -0,0 +1,39 @@
+var bill = document.querySelector("#billAmount");
+var cashes = document.querySelector("#cashGiven");
+var notesGiven = document.querySelectorAll(".notes");
+var clickButton = document.querySelector("#check");
+const message = document.querySelector("#error");
+const availableNotes = ["2000", "500", "100", "50", "10", "5", "1"];
+clickButton.addEventListener("click", function validation() {
+    hideMessage();
+    if (bill.value > 0) {
+        if (cashes.value >= bill.value) {
+            const totalAmount = cashes.value - bill.value;
+            calculateChange(totalAmount);
+        }
+        else {
+            show("Read the bill right!");
+        }
+    }
+    else if (typeof bill.value === 'string') {
+        show("I don't know anything other than cash!");
+    }
+    else {
+        show("Invalid input");
+    }
+})
+function calculateChange(totalAmount) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberTotal = Math.trunc(totalAmount / availableNotes[i]);
+
+        totalAmount = totalAmount % availableNotes[i];
+        notesGiven[i].innerText = numberTotal;
+    }
+}
+function hideMessage() {
+    message.style.display = "none";
+}
+function show(msg) {
+    message.style.display = "block";
+
+}
